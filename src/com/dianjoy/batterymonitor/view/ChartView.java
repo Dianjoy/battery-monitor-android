69644afdb68fc2005d69993d3f0ffb412c1af845
gaoyan10 <gaoyansansheng@gmail.com>
2013-11-26 16:59:58 +0800

    图表

---
+++ src/com/dianjoy/batterymonitor/view/ChartView.java
@@ -0,0 +1,14 @@
+package com.dianjoy.batterymonitor.view;
+
+import android.content.Context;
+import android.view.View;
+
+public class ChartView extends View{
+	public ChartView(Context context) {
+		super(context);
+		// TODO Auto-generated constructor stub
+	}
+	public int XPoint, YPoint, XScale, YScale;
+	public int XLength, YLength;
+	
+}
